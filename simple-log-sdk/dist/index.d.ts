e4e76c2fc4adab17f31d62f6b1fd4a863651e7bc
Ayush Maurya <ayush.maurya@agilonhealth.com>
2025-04-10 00:08:01 +0530

    Updated backend scripts

--- simple-log-sdk/dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { SimpleLogTracker } from "./core/tracker";
-export { TrackerProvider as SimpleLogTrackerProvider } from "./react/TrackerProvider";
-export { useTracker as useSimpleLogTracker } from "./react/useTracker";
-export type { InitOptions } from "./core/tracker";
