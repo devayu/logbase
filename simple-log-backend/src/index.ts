d2e17dc419effcab822960dea1a31c64f2240a5c
Ayush Maurya <ayush.maurya@agilonhealth.com>
2025-04-14 01:18:15 +0530

    backend : added cors support

--- simple-log-backend/src/index.ts
+++ simple-log-backend/src/index.ts
@@ -3,10 +3,17 @@ import app from "./app";
 import heathRoute from "./routes/health";
 import projectRoute from "./routes/project";
 import trackerRouter from "./routes/tracker";
-
+import cors from "cors";
 dotenv.config();
-
 const PORT = process.env.PORT || 3090;
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(heathRoute);
 app.use(projectRoute);
 app.use(trackerRouter);
